58a6ac2164d8d1eb7d2cc5ea7b0dfac477761a12
mohammdowais <mohdowais1920@gmail.com>
2022-11-02 16:26:24 +0530

    updated

--- registerform/src/App.js
+++ registerform/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <RegistrationForm/> 
+      {/* <RegistrationForm/>  */}
       {/* <Login/> */}
       {/* <ChangePass/> */}
       {/* <Signup/> */}
--- registerform/src/components/angelprofile.jsx
+++ registerform/src/components/angelprofile.jsx
@@ -4,10 +4,10 @@ class Profile extends Component{
     render(){
         return(
             <div>
-                <div className="row">
-                    <div className="col"></div>
-                    <div className="col"></div>
-                    <div className="col"></div>
+                <div className="grid-container">
+                    <div className="item1">Menu</div>
+                    <div className="item2">Main</div>  
+                    <div className="item3">Footer</div>
                 </div>
             </div>
         )
