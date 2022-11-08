d41f7e46ad06360ca1eb000d84b97b1a0104592e
mohammdowais <mohdowais1920@gmail.com>
2022-11-08 13:15:31 +0530

    added Service folder and loginService js file

---
+++ registerform/src/services/LoginService.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+const ANGEL_POST_URL ="";
+
+class LoginService{
+    postRequestData(angel){
+        return ({
+
+        })
+    }
+}
