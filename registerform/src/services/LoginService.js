116366c5466c4a0e473ad47bbf530b31cda23c48
arjunverma06 <arjunverma5319@gmail.com>
2022-11-16 09:52:11 +0530

    worked on api

--- registerform/src/services/LoginService.js
+++ registerform/src/services/LoginService.js
@@ -1,18 +1,28 @@
 import axios from 'axios'
-const ANGEL_POST_URL ="http://localhost:8080/api/loginhelper/login";
+const url ="http://localhost:8080/api/loginhelper/login"
+
 
 class LoginService{
     postRequestData(angel){
         console.log(angel)
-        return axios({
-            method:'post',
-            url:ANGEL_POST_URL,
-            data:{
-                email:angel.email,
-                pasword:angel.password
-            }
+            // return axios({
+            //     method:'post',
+            //     url:url,
+            //     data:{
+            //         email:angel.email,
+            //         pasword:angel.password
+            //     }
 
-        })
+            // })
+
+            axios.post(url, {
+      email: angel.email,
+      password: angel.password
+    })
+    .then((response) => {
+      console.log(response);
+    });
+        
     }
 }
 export default new LoginService()
