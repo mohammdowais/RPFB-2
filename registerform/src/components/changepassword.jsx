4f260a2090b557a6621a6756c549e56e196a845f
mohammdowais <mohdowais1920@gmail.com>
2022-11-20 12:27:50 +0530

    updated signup and angelprofile

--- registerform/src/components/changepassword.jsx
+++ registerform/src/components/changepassword.jsx
@@ -1,20 +1,16 @@
-import React,{useState} from 'react'
-import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom'
-import { yupResolver } from '@hookform/resolvers/yup'
-import axios from 'axios'
-import * as Yup from 'yup'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
+import * as Yup from "yup";
 
-const styles = {
-  responsive: {
-    maxWidth:"350px",
-  }
-}
 export default function ChangePass() {
+  // validation
   const formSchema = Yup.object().shape({
     password: Yup.string()
-      .required('Password is mendatory')
-      .max(32,'Password should be less than 32 characters')
+      .required("Password is mendatory")
+      .max(32, "Password should be less than 32 characters")
       .matches(
         /[.*[A-Z].*]{0,}/,
         "Must Contain at least one Uppercase Character"
@@ -23,64 +19,64 @@ export default function ChangePass() {
         /[.*[a-z].*]{0,}/,
         "Must Contain at least one Lowercase Character"
       )
-      .matches(
-        /[.*[0-9].*]{0,}/,
-        "Must Contain at least one Number Character"
-      )
-      .min(5, 'Password must be at 5 char long'),
+      .matches(/[.*[0-9].*]{0,}/, "Must Contain at least one Number Character")
+      .min(5, "Password must be at 5 char long"),
     confirmpassword: Yup.string()
-      .required('Password is mendatory')
-      .oneOf([Yup.ref('password')], 'Passwords does not match'),
-  })
-  const navigate = useNavigate();
-  // const navigateMethod=(event)=>{
-  //   navigate("/login");
-  // }
+      .required("Password is mendatory")
+      .oneOf([Yup.ref("password")], "Passwords does not match"),
+  });
 
-  const formOptions = { resolver: yupResolver(formSchema) }
-  const { register, handleSubmit, reset, formState } = useForm(formOptions)
-  const { errors } = formState
-  const [respX,setRespX] =useState('');
-  const LOGIN_POST_URL ="http://localhost:8080/api/loginhelper/changepassword"
+  const navigate = useNavigate();
+  const formOptions = { resolver: yupResolver(formSchema) };
+  const { register, handleSubmit, formState } = useForm(formOptions);
+  const { errors } = formState;
+  const [setRespX] = useState("");
+  const LOGIN_POST_URL = "http://localhost:8080/api/loginhelper/changepassword";
+
   function onSubmit(angel) {
-    console.log(JSON.stringify(angel, null, 4))
-    
+    console.log(JSON.stringify(angel, null, 4));
+
     axios({
-      method: 'post',
+      method: "post",
       url: LOGIN_POST_URL,
       data: {
-              // email: angel.email,
-              email:"a@gmail.com",
-              password: angel.password,
-              oldpassword: angel.oldpassword
-              }
-  }).then(res =>{
-      console.log(" in "+res.data);
-      if(res.data === "Password changed"){
+        // email: angel.email,
+        email: "a@gmail.com",
+        password: angel.password,
+        oldpassword: angel.oldpassword,
+      },
+    }).then((res) => {
+      console.log(" in " + res.data);
+      if (res.data === "Password changed") {
         navigate("/angelprofile");
-      }
-      else{
-        console.log("login is this")
+      } else {
+        console.log("login is this");
       }
       setRespX(res.data);
-    })
+    });
   }
+  const styles = {
+    responsive: {
+      maxWidth: "350px",
+    },
+  };
   return (
-    <div >
-      <form 
-      style={styles.responsive}
-      onSubmit={handleSubmit(onSubmit)} 
-      className="row gy-3 bg-light p-3 mx-auto mt-5 rounded">
+    <div>
+      <form
+        style={styles.responsive}
+        onSubmit={handleSubmit(onSubmit)}
+        className="row gy-3 bg-light p-3 mx-auto mt-5 rounded"
+      >
         <div className="form-group col-12 mb-3 mt-3">
-              <h4>Change Password</h4>
-           </div>
-           <div className="form-group col-12">
+          <h4>Change Password</h4>
+        </div>
+        <div className="form-group col-12">
           <label>Old Password</label>
           <input
             name="password"
             type="password"
-            {...register('oldPassword')}
-            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+            {...register("oldPassword")}
+            className={`form-control ${errors.password ? "is-invalid" : ""}`}
             placeholder="Enter New password"
           />
           <div className="invalid-feedback">{errors.password?.message}</div>
@@ -90,8 +86,8 @@ export default function ChangePass() {
           <input
             name="password"
             type="password"
-            {...register('password')}
-            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+            {...register("password")}
+            className={`form-control ${errors.password ? "is-invalid" : ""}`}
             placeholder="Enter New password"
           />
           <div className="invalid-feedback">{errors.password?.message}</div>
@@ -101,20 +97,18 @@ export default function ChangePass() {
           <input
             name="confirmPwd"
             type="text"
-            {...register('confirmpassword')}
-            className={`form-control ${errors.confirmPwd ? 'is-invalid' : ''}`}
+            {...register("confirmpassword")}
+            className={`form-control ${errors.confirmPwd ? "is-invalid" : ""}`}
             placeholder="Confirm New password"
           />
           <div className="invalid-feedback">{errors.confirmPwd?.message}</div>
         </div>
         <div className="col-12">
-          <button type="submit" className="btn btn-primary btn-block" 
-          // onClick={navigateMethod}
-          >
+          <button type="submit" className="btn btn-primary btn-block">
             Submit
           </button>
         </div>
       </form>
     </div>
-  )
+  );
 }
--- registerform/src/components/signup.jsx
+++ registerform/src/components/signup.jsx
@@ -1,108 +1,139 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 const styles = {
   responsive: {
-    maxWidth:"350px",
-  }
-}
+    maxWidth: "350px",
+  },
+};
 export default function Signup() {
-
-    const {register, handleSubmit, formState:{errors} } = useForm();
-    const [respX,setRespX] =useState('');
-    const LOGIN_POST_URL ="http://localhost:8080/api/signup"
-    const onSubmit = (data)=>{
-      console.log(data);
-      console.log(data.phoneNumber)
-      axios({
-        method: 'post',
-        url: LOGIN_POST_URL,
-        data: {
-                email: data.emailId,
-                firstName: data.firstName,
-                lastName:data.lastname,
-                contact_no:data.phoneNumber,
-                // password:"h1K@"
-                }
-    }).then(res =>{
-        console.log(" in "+res.data);
-        if(res.data === "Signup Successfull"){
-          navigate("/login");
-        }
-        else{
-          console.log("Looks Like you are already a user please signup again")
-        }
-        setRespX(res.data);
-      })
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [respX, setRespX] = useState("");
+  const LOGIN_POST_URL = "http://localhost:8080/api/signup";
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log(data.phoneNumber);
+    axios({
+      method: "post",
+      url: LOGIN_POST_URL,
+      data: {
+        email: data.emailId,
+        firstName: data.firstName,
+        lastName: data.lastname,
+        contact_no: data.phoneNumber,
+        // password:"h1K@"
+      },
+    }).then((res) => {
+      console.log(" in " + res.data);
+      if (res.data === "Signup Successfull") {
+        alert("Login Password has been send to email");
+        navigate("/login");
+      } else {
+        console.log("Looks Like you are already a user please signup again");
+      }
+      setRespX(res.data);
+    });
+  };
   const navigate = useNavigate();
-  const navigateMethod=(event)=>{
+  const navigateMethod = (event) => {
     // navigate("/login");
-  }
+  };
 
-    return (
-      
-      <form 
+  return (
+    <form
       style={styles.responsive}
       className="row gy-3 bg-light p-3 mx-auto mt-5 rounded"
-      onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-group col-12 mb-3 mt-3">
-              <h4>Sign Up</h4>
-            </div>
-            <div className="col-6" >
-                <label htmlFor="firstName" className="form-label ">First Name:</label>
-                <input type="text" 
-                className="form-control mb-1" {...register("firstName",{required:true,maxLength: 25})} 
-                placeholder='James'  />
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="form-group col-12 mb-3 mt-3">
+        <h4>Sign Up</h4>
+      </div>
+      <div className="col-6">
+        <label htmlFor="firstName" className="form-label ">
+          First Name:
+        </label>
+        <input
+          type="text"
+          className="form-control mb-1"
+          {...register("firstName", { required: true, maxLength: 25 })}
+          placeholder="James"
+        />
 
-                {errors.firstName && <p className="text-danger muted">Please Valid First Name</p>}
-            </div>
-            <div className="col-6">
-                <label htmlFor="lastname" className="form-label "> Last Name:</label>
-                <input type="text" 
-                className="form-control mb-1" 
-                {...register("lastname",{required:true,maxLength:25})} 
-                placeholder='Bond' pattern="[a-zA-Z ]{3,50}" />
+        {errors.firstName && (
+          <p className="text-danger muted">Please Valid First Name</p>
+        )}
+      </div>
+      <div className="col-6">
+        <label htmlFor="lastname" className="form-label ">
+          {" "}
+          Last Name:
+        </label>
+        <input
+          type="text"
+          className="form-control mb-1"
+          {...register("lastname", { required: true, maxLength: 25 })}
+          placeholder="Bond"
+          pattern="[a-zA-Z ]{3,50}"
+        />
 
-            {errors.firstName && <p className="text-danger muted">Please Valid Last Name</p>}
-            </div>
-        
-        
-            <div className="col-12">
-                <label htmlFor="emailId" className="form-label ">Email:</label>
-                <input 
-                className="form-control mb-1" 
-                {...register("emailId",{
-                  required:true,
-                  pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ,
-                })}  
-                placeholder='jamesbond@angel.com' />
+        {errors.firstName && (
+          <p className="text-danger muted">Please Valid Last Name</p>
+        )}
+      </div>
 
-            {errors.emailId && <p className="text-danger muted">Please enter valid email</p>}
-            </div>
-        
-        
-            <div className="col-12">
-                <label htmlFor="phoneNumber" className="form-label ">Contact Number:</label>
-                <input type="tel" 
-                className="form-control mb-3" 
-                {...register("phoneNumber",{
-                required:true,
-                pattern:/^[0-9]{10}$/,
-                })} 
-                placeholder='Contact number' 
-                />
-                {errors.phoneNumber && <p className="text-danger muted">Please Enter Valid Number</p>}
-            </div>
-        
-        
-            <div className="col-12">
-                <button type="submit" className='btn btn-primary btn-block my-1 form-control ' onClick={navigateMethod}>Sign Up</button>
-            </div>
-        <script src="tele.js">
-          
-        </script>
-      </form>
-    )
+      <div className="col-12">
+        <label htmlFor="emailId" className="form-label ">
+          Email:
+        </label>
+        <input
+          className="form-control mb-1"
+          {...register("emailId", {
+            required: true,
+            pattern:
+              /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          })}
+          placeholder="jamesbond@angel.com"
+        />
+
+        {errors.emailId && (
+          <p className="text-danger muted">Please enter valid email</p>
+        )}
+      </div>
+
+      <div className="col-12">
+        <label htmlFor="phoneNumber" className="form-label ">
+          Contact Number:
+        </label>
+        <input
+          type="tel"
+          className="form-control mb-3"
+          {...register("phoneNumber", {
+            required: true,
+            pattern: /^[0-9]{10}$/,
+          })}
+          placeholder="Contact number"
+        />
+        {errors.phoneNumber && (
+          <p className="text-danger muted">Please Enter Valid Number</p>
+        )}
+      </div>
+
+      <div className="col-12">
+        <button
+          type="submit"
+          className="btn btn-primary btn-block my-1 form-control "
+          onClick={navigateMethod}
+          // onclick={alert("Login Password has been send to email")}
+        >
+          Sign Up
+        </button>
+      </div>
+      <script src="tele.js"></script>
+    </form>
+  );
 }
